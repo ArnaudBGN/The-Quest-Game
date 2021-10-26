33beab1a8df58b40d346eacab96875045f965d82
Benjamin <b.girardpro@gmail.com>
2021-10-26 12:07:25 +0200

    Create GameScene component

---
+++ src/components/GameScene.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function GameScene() {
+  return (
+    <div className="GameScene-container">
+      <div className="GameScene-scene">
+        <img src="" alt="" />
+      </div>
+      <div className="GaneScene-text"></div>
+      <div className="GameScene-choices"></div>
+    </div>
+  );
+}
+
+export default GameScene;
