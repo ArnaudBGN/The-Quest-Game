2e2399bafb8f1b1fc8156e31e52970a55eede443
Benjamin <b.girardpro@gmail.com>
2021-10-26 14:09:21 +0200

    Create GameDialog css file

---
+++ src/components/GameDialog.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+import './style/GameDialog.css';
+
+function GameDialog({ messages }) {
+  const [currentMessage, setCurrentMessage] = useState(0);
+  const handleClickNext = () => {
+    if (currentMessage < messages.length - 1) {
+      setCurrentMessage(currentMessage + 1);
+    } else {
+      setCurrentMessage(0);
+    }
+  };
+
+  return (
+    <div className="DialogBox-container">
+      <div className="DialogBox-title">Dialog title</div>
+      <div className="DialogBox-Footer">
+        <button onClick={handleClickNext} className="DialogBox-Next">
+          Next &gt;
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default GameDialog;
