d138e5ae3a5599961f2f02b67266fa091b049580
Benjamin <b.girardpro@gmail.com>
2021-10-27 09:24:07 +0200

    Create Data object that will contains all the element for the story

---
+++ src/data/Story.js
@@ -0,0 +1,20 @@
+const story = [
+  {
+    id: 0,
+    image: '',
+    route: [
+      {
+        action: '',
+        require: '',
+        text: [],
+        choice: {
+          type: '',
+          choices: [],
+        },
+        nextId: 0,
+      },
+    ],
+  },
+];
+
+export default story;
