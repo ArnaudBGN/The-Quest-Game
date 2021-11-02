b291b7e06db26358b33c21e62321fa99a9aed2f4
Benjamin <b.girardpro@gmail.com>
2021-11-02 21:28:15 +0100

    Redispatch space for Game Scene element

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -21,7 +21,7 @@ function GamePage() {
       <div className={styles.GamePageScene}>
         <img src={currentStory[0].image} alt="" />
       </div>
-      <div className={styles.GagstneSceneText}>
+      <div className={styles.GamePageSceneText}>
         <GameDialog messages={currentStory[0].route.text} currentMessage={currentMessage} setCurrentMessage={setCurrentMessage} />
       </div>
       <div className={styles.GamePageChoices}>
