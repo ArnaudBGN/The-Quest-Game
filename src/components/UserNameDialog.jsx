9a85aa74bc213445d97bf8719cff1d8967716b6c
Benjamin <b.girardpro@gmail.com>
2021-11-02 21:22:12 +0100

    Modify style to make modal appears in the middle of the homepage

--- src/components/UserNameDialog.jsx
+++ src/components/UserNameDialog.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Modal from './Modal';
 
+import styles from './style/UserNameDialog.module.css';
+
 function UserNameDialog({ revele, toggle, goToCharacterSelect, setUserName }) {
   return (
-    <div>
+    <div className={styles.UserNameDialog}>
       <Modal revele={revele} toggle={toggle} goToCharacterSelect={goToCharacterSelect} setUserName={setUserName} />
     </div>
   );
