13bd61a292205cb3dd6ec817b9e4973c20f97e84
Benjamin <b.girardpro@gmail.com>
2021-11-14 17:23:53 +0100

    Add new routes for the story

--- src/data/Story.js
+++ src/data/Story.js
@@ -35,8 +35,8 @@ const story = {
       action: 'lookAround',
       require: '',
       text: [
-        `Vous contenplez les lieux qui vous entoure. Cela vous semble familier mais vous ne vous souvenez toujours pas d'où vous êtes`,
-        `Vous êtes au beau milieu d'un sacré bordel. Des corps étalés au sol, des habits éparpillés un peu partout et surtout une odeur qui combine une mélange subtile de vomit et d'alchol ,Tout d'un coup, vous êtes pris d'un sursaut de panique et vous ne sentez plus votre sac à dos peser sur vos épaules`,
+        `Vous contemplez les lieux qui vous entoure. Cela vous semble familier mais vous ne vous souvenez toujours pas d'où vous êtes`,
+        `Vous êtes au beau milieu d'un sacré bordel. Des corps étalés au sol, des habits éparpillés un peu partout et surtout une odeur qui combine un mélange subtile de vomit et d'alchol. Tout à coup, vous êtes pris d'un sursaut de panique et vous ne sentez plus votre sac à dos peser sur vos épaules`,
         `Que faites vous ?`,
       ],
       choices: [
@@ -44,24 +44,98 @@ const story = {
           type: 'button',
           choiceText: 'Fouiller la zone',
           choiceMade: 'search',
-          nextId: '1-1',
+          nextId: '1-1a',
         },
         {
           type: 'button',
           choiceText: 'Paniquer et tourner en rond en levant les bras',
           choiceMade: 'panic',
-          nextId: '2',
+          nextId: '1-1b',
         },
       ],
     },
   },
-  '1-1': {
+  '1-1a': {
     image: 'https://picsum.photos/400/306',
     route: {
       action: 'search',
       text: [
         `Vous fouillez les alentours. Vous regarder un peu partout dans ce qui resemble à un campement mais en beaucoup plus grand.`,
         `Beaucoup, beaucoup.... BEAUCOUP plus grand.`,
+        `Vous vous retrouvez devant une tente qui vous rappel quelque chose dans un vague souvenir.`,
+        `Cette tente se trouve également posée à côté d'un petit bosquet`,
+        `Que fouillez-vous ?`,
+      ],
+      choices: [
+        {
+          type: 'button',
+          choiceText: 'Fouiller la tente',
+          choiceMade: 'tent',
+          nextId: '1-1a-1a',
+        },
+        {
+          type: 'button',
+          choiceText: 'Fouiller le bosquet',
+          choiceMade: 'forest',
+          nextId: '1-1a-1b',
+        },
+      ],
+    },
+  },
+  '1-1a-1a': {
+    image: 'https://picsum.photos/400/308',
+    route: {
+      action: 'tent',
+      text: [
+        `Vous entrez dans la tente.`,
+        `Quel bordel là dedans ! L'odeur vous rappel quelque chose...`,
+        `Après avoir sniffer, vous remarquez que vous portez la même odeur.`,
+        `Malgré le dégoût prononcé qui peut se lire sur votre visage, vous êtes confiant que votre barda se trouve ici`,
+        `IL EST LAAAAAAAAA !!! Vous êtes tellement que vous hurlez de joie.`,
+        `Manque de bol, votre sac se trouve à côté d'un lit où dort le genre de gars qu'il ne faut mieux pas réveiller.`,
+        ``,
+      ],
+    },
+  },
+  '1-1b': {
+    image: 'https://picsum.photos/400/307',
+    route: {
+      action: 'panic',
+      text: [
+        `Vous avez le palpitant qui s'emballe, vous vous mettez à transpirer à grosse goûte`,
+        `Vous vous dites: `,
+        `C'est un CAUCHEMAAAAAAAAAAAARD !!!!'`,
+        `Il faut absolument que je retrouve mes affaires !`,
+        `Que faites-vous ?`,
+      ],
+      choices: [
+        {
+          type: 'button',
+          choiceText: 'Courrir vite et loin',
+          choiceMade: 'run',
+          nextId: '2-1',
+        },
+        {
+          type: 'button',
+          choiceText: 'Se resaisir et fouillez les lieux',
+          choiceMade: 'search',
+          nextId: '1-1a',
+        },
+      ],
+    },
+  },
+  '2-1': {
+    image: 'https://picsum.photos/400/309',
+    route: {
+      action: 'end',
+      text: [`Vous Courrez vite, le plus vite que vous n'ayez jamais courru.`, `Dommage que l'aventure se termine déjà.`],
+      choices: [
+        {
+          type: 'button',
+          choiceText: 'Fin',
+          nextId: 'End',
+          state: false,
+        },
       ],
     },
   },
