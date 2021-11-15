d17418ca2c271cbaa7856d3f1a8b37e4d356f1e8
Arnaud BEGIN <arnaud-begin_student2021@wilder.school>
2021-11-15 11:06:55 +0100

    images folder modified and fully functional, responsive playerSelect page

--- src/components/CharDescTabs.jsx
+++ src/components/CharDescTabs.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 import styles from '../components/style/CharDescTabs.module.css';
+import { getCharacterData } from '../data/CharacterData';
 
 export default function CharDescTabs({ card }) {
   const [stateTabs, setStateTabs] = useState(1);
@@ -13,53 +14,67 @@ export default function CharDescTabs({ card }) {
   };
 
   const [Characters, SetCharacters] = useState([]);
+  const [characterData, setCharacterData] = useState({});
 
   useEffect(() => {
     axios
       .get(`https://www.dnd5eapi.co${card.url}`)
       .then((res) => res.data)
       .then((data) => SetCharacters(data));
+
+    getCharacterData(card.index).then((character) => setCharacterData(character));
   }, []);
 
   return (
-    <div>
-      <div className={styles.TabsContent}>
-        <button type="button" onClick={goDescTab} className={`Tabs ${stateTabs === 1 ? 'active' : ''}`}>
-          Character Description
-        </button>
-        <button type="button" onClick={goAttrTab} className={`Tabs ${stateTabs === 2 ? 'active' : ''}`}>
-          Character Attributes
-        </button>
-      </div>
+    <>
+      {Characters && characterData && (
+        <div>
+          <div className={styles.TabsContent}>
+            <button type="button" onClick={goDescTab} className={`Tabs ${stateTabs === 1 ? 'active' : ''}`}>
+              Character Description
+            </button>
+            <button type="button" onClick={goAttrTab} className={`Tabs ${stateTabs === 2 ? 'active' : ''}`}>
+              Character Attributes
+            </button>
+          </div>
 
-      <div className={styles.CharacterCardContainer}>
-        {stateTabs === 1 ? (
-          <div className={styles.CharacterCardDescription}>
-            <p>
-              <em>Race</em> : {Characters.name}
-            </p>
-            <p>
-              <em>Description</em> : {Characters.alignment}
-            </p>
-            <p>
-              <em>Size</em>: {Characters.size}
-            </p>
+          <div className={styles.CharacterCardContainer}>
+            {stateTabs === 1 ? (
+              <div className={styles.CharacterCardDescription}>
+                <p>
+                  <em>Race</em> : {Characters.name}
+                </p>
+                <p>
+                  <em>Description</em> : {Characters.alignment}
+                </p>
+                <p>
+                  <em>Size</em> : {Characters.size}
+                </p>
+                <p>
+                  <em>Speed</em> : {Characters.speed}
+                </p>
+                <p>
+                  <em>Strength</em> : {characterData.strength}
+                </p>
+                <p>
+                  <em>Intelligence</em> : {characterData.intelligence}
+                </p>
+              </div>
+            ) : (
+              <div className={styles.CharacterCardAttributes}>
+                <ul>
+                  <em>Spells</em> :<li>{characterData.spell[0].name}</li>
+                  <em>Description</em> : {characterData.spell[0].description}
+                  <li>{characterData.spell[1].name}</li>
+                  <em>Description</em> : {characterData.spell[1].description}
+                  <li>{characterData.spell[2].name}</li>
+                  <em>Description</em> : {characterData.spell[2].description}
+                </ul>
+              </div>
+            )}
           </div>
-        ) : (
-          <div className={styles.CharacterCardAttributes}>
-            <p>
-              <em>Speed</em> : {Characters.speed}
-            </p>
-            <p>Strength: 30</p>
-            <ul>
-              Spells:
-              <li>Spell 1</li>
-              <li>Spell 2</li>
-              <li>Spell 3</li>
-            </ul>
-          </div>
-        )}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/data/CharacterData.js
+++ src/data/CharacterData.js
@@ -1,37 +1,122 @@
-const objectIMG = [
-  {
+const characterData = {
+  'half-orc': {
     race: 'half-orc',
-    img: './src/components/images/Perso_Half-Orc.png',
+    img: './src/components/images/Half-orc.png',
     strength: 60,
-    spells: [
+    intelligence: 10,
+    spell: [
       {
-        index: 1,
+        id: 1,
         name: 'Polymorph',
-        description: 'Can change his appearance to trick his opponent',
+        description:
+          'This spell transforms a creature that you can see within range into a new form. An unwilling creature must make a wisdom saving throw to avoid the effect. A shape-shifter automatically succeeds on this saving throw.',
+        damage: -5,
       },
       {
-        index: 2,
-        name: 'Slave-banner',
+        id: 2,
+        name: 'Earthquake',
         description:
-          'This spell requires a rod or staff. By stamping one end against the ground and casting the spell, it conjures and unfurls a war banner attached to the head of the staff',
+          'You create a seismic disturbance at a point on the ground that you can see within range. For the duration, an intense tremor rips through the ground in a 100-foot-radius circle centered on that point and shakes creatures and structures in contact with the ground in that area.',
+        damage: -20,
+      },
+      {
+        id: 3,
+        name: 'Circle of Death',
+        description:
+          'A sphere of negative energy ripples out in a 60-foot radius sphere from a point within range. Each creature in that area must make a constitution saving throw.',
+        damage: -25,
       },
     ],
   },
-  {
+  dwarf: {
     race: 'dwarf',
-    img: './src/components/images/Perso-Dwarf.png',
+    img: './src/components/images/Dwarf.png',
     strength: 35,
+    intelligence: 30,
+    spell: [
+      {
+        id: 1,
+        name: 'Arcane Sword',
+        description: 'You create a sword-shaped plane of force that hovers within range.',
+        damage: -5,
+      },
+      {
+        id: 2,
+        name: 'Blade Barrier',
+        description: 'You create a vertical wall of whirling, razor-sharp blades made of magical energy',
+        damage: -20,
+      },
+      {
+        id: 3,
+        name: 'Control Weather',
+        description:
+          "You take control of the weather within 5 miles of you for the duration. You must be outdoors to cast this spell. Moving to a place where you don't have a clear path to the sky ends the spell early.",
+        damage: -10,
+      },
+    ],
   },
-  {
+  elf: {
     race: 'elf',
-    img: './src/components/images/Perso-Elf.png',
-    strength: 25,
-  },
-  {
-    race: 'human',
-    img: './src/components/images/Perso-Human.png',
+    img: './src/components/images/Elf2.png',
     strength: 45,
+    intelligence: 50,
+    spell: [
+      {
+        id: 1,
+        name: 'Fireball',
+        description:
+          'A bright streak flashes from your pointing finger to a point you choose within range and then blossoms with a low roar into an explosion of flame',
+        damage: -15,
+      },
+      {
+        id: 2,
+        name: 'Wall of Fire',
+        description:
+          'You create a wall of fire on a solid surface within range. You can make the wall up to 60 feet long, 20 feet high, and 1 foot thick, or a ringed wall up to 20 feet in diameter, 20 feet high, and 1 foot thick.',
+        damage: -20,
+      },
+      {
+        id: 3,
+        name: 'Acid Arrow',
+        description:
+          'A shimmering green arrow streaks toward a target within range and bursts in a spray of acid. Make a ranged spell attack against the target.',
+        damage: -10,
+      },
+    ],
   },
-];
+  human: {
+    race: 'human',
+    img: './src/components/images/Human.png',
+    strength: 40,
+    intelligence: 30,
+    spell: [
+      {
+        id: 1,
+        name: 'Disguise Self',
+        description:
+          'You make yourself--including your clothing, armor, weapons, and other belongings on your person--look different until the spell ends or until you use your action to dismiss it. You can seem 1 foot shorter or taller and can appear thin, fat, or in between.',
+        damage: -5,
+      },
+      {
+        id: 2,
+        name: 'Stinking Cloud',
+        description:
+          'You create a 20-foot-radius sphere of yellow, nauseating gas centered on a point within range. The cloud spreads around corners, and its area is heavily obscured.',
+        damage: -25,
+      },
+      {
+        id: 3,
+        name: 'Vicious Mockery',
+        description: 'You unleash a string of insults laced with subtle enchantments at a creature you can see within range.',
+        damage: -15,
+      },
+    ],
+  },
+};
 
-export default objectIMG;
+export const getCharacterData = (id) => {
+  // eslint-disable-next-line no-undef
+  return new Promise((resolve) => {
+    resolve(characterData[id]);
+  });
+};
