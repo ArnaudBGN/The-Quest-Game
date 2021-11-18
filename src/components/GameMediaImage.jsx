ce80c1ff2461e4f0ae6cfe5b56a0bef8ab831632
sarah <minipouce75@gmail.com>
2021-11-18 10:47:15 +0100

    change background-image dialogue

--- src/components/GameMediaImage.jsx
+++ src/components/GameMediaImage.jsx
@@ -5,7 +5,7 @@ import styles from './style/GameMediaImage.module.css';
 function GameMediaImage({ src }) {
   return (
     <div className={styles.GameMediaImage}>
-      <img src={src} alt="" style={{ width: '100%', height: '100%' }} />
+      <img src={src} alt="" />
     </div>
   );
 }
